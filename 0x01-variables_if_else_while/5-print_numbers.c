20655b35407eedec6cfc23b2d18b9a5bd0b1dc3b
Natnael Abebe <natnaelabebe177@hotmail.com>
2023-03-17 09:11:32 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function of a program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
