807ee6591b0110e8a37d1ea9d1d56474fd83a4b1
Natnael Abebe <natnaelabebe177@hotmail.com>
2023-03-17 09:20:44 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function of a program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
