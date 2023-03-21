74f4d0d785cdd242357196c0ad7a6c8dd0fbef82
Natnael Abebe <natnaelabebe177@hotmail.com>
2023-03-21 07:19:03 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - addition of two numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * times_table - time fun
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
