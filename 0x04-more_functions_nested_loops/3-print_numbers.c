8107233c67b2fce42b52cf39ebf8e2072ef82feb
Natnael Abebe <natnaelabebe177@hotmail.com>
2023-03-23 03:37:49 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers from 0 to 9
  * Return: success
  */
 
 void print_numbers(void)
-{
-	char x;
 
-	for (x = '0' ; x <= '9' ; x++)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
 	{
-		_putchar(x);
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
